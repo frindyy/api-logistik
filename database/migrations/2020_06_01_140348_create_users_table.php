170f0222f8e3d5f4ffe6f862a6fd8588eeda21c0
frindyy <frindy.pratama090693@gmail.com>
2020-06-01 22:01:14 +0700

    Add tables database

---
+++ database/migrations/2020_06_01_140348_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('identity_id');
+            $table->char('gender',1)->comment('0: female, 1: male');
+            $table->text('address');
+            $table->string('photo');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone_number',20);
+            $table->string('api_token');
+            $table->char('role',1)->comment('0: admin, 1: driver, 2: user');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
